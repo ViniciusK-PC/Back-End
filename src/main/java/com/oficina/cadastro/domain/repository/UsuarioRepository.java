e746d0074b3a3ff4edafd7210e0520e18bd21f38
ViniciusK-PC <viniciuskb36@gmail.com>
2026-01-09 05:01:12 -0300

    fix: FIX

--- src/main/java/com/oficina/cadastro/domain/repository/UsuarioRepository.java
+++ src/main/java/com/oficina/cadastro/domain/repository/UsuarioRepository.java
@@ -10,10 +10,13 @@ public interface UsuarioRepository extends org.springframework.data.jpa.reposito
         Optional<Usuario> findByEmail(String email);
 
         @org.springframework.data.jpa.repository.Query(value = "SELECT * FROM usuarios u WHERE " +
-                        "(:nome IS NULL OR LOWER(u.nome) LIKE LOWER(CONCAT('%', :nome, '%'))) AND " +
-                        "(:email IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%'))) AND " +
-                        "(:perfil IS NULL OR Cast(u.perfil as varchar) = :#{#perfil?.name()}) AND " +
-                        "(:ativo IS NULL OR u.ativo = :ativo)", nativeQuery = true)
+                        "(CAST(:nome AS varchar) IS NULL OR LOWER(u.nome) LIKE LOWER(CONCAT('%', CAST(:nome AS varchar), '%'))) AND "
+                        +
+                        "(CAST(:email AS varchar) IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', CAST(:email AS varchar), '%'))) AND "
+                        +
+                        "(CAST(:#{#perfil?.name()} AS varchar) IS NULL OR u.perfil = CAST(:#{#perfil?.name()} AS varchar)) AND "
+                        +
+                        "(CAST(:ativo AS boolean) IS NULL OR u.ativo = CAST(:ativo AS boolean))", nativeQuery = true)
         List<Usuario> findByFiltros(
                         @org.springframework.data.repository.query.Param("nome") String nome,
                         @org.springframework.data.repository.query.Param("email") String email,
