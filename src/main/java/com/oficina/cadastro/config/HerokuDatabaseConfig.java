7a7ebee69e2264e34371ba7c8996ad41d9f4460d
ViniciusK-PC <viniciuskb36@gmail.com>
2026-01-09 00:03:00 -0300

    fix: Deploy no Postgres

---
+++ src/main/java/com/oficina/cadastro/config/HerokuDatabaseConfig.java
@@ -0,0 +1,39 @@
+package com.oficina.cadastro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("heroku")
+public class HerokuDatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        if (databaseUrl == null) {
+            throw new RuntimeException("DATABASE_URL environment variable is not set");
+        }
+
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+                + "?sslmode=require";
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName("org.postgresql.Driver");
+
+        return new HikariDataSource(config);
+    }
+}
